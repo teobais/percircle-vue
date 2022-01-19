ef93cf35298cb2b40d720d7bb606d65b3b03cb05
Kevin Miller <miller.kevin1988@gmail.com>
2022-01-18 21:49:58 -0500

    Removed gt50 nonsense, converted to svgs

--- src/components/percircle.vue
+++ src/components/percircle.vue
@@ -2,28 +2,35 @@
   <div
     :class="[
       'percircle',
-      { animate, gt50: addGt50, perclock, perdown, initialized: ready },
+      { animate, perclock, perdown, initialized: initialized },
     ]"
     @mouseenter="startHover"
     @mouseleave="stopHover"
     @click="timerReset"
   >
     <span :style="{ color: textColor }">{{ displayText }}</span>
-    <div class="slice">
-      <div
-        class="bar"
-        :style="{
-          'border-color': progressBarColor,
-          transform: `rotate(${rotationDegrees}deg)`,
-        }"
-      ></div>
-      <div
-        class="fill"
-        :style="{
-          'border-color': progressBarColor,
-        }"
-      ></div>
-    </div>
+
+    <svg viewBox="0 0 120 120">
+      <circle
+        class="filler-bar circle-bar"
+        cx="60"
+        cy="60"
+        r="54"
+        fill="none"
+      />
+
+      <circle
+        class="progress-bar circle-bar"
+        cx="60"
+        cy="60"
+        r="54"
+        fill="none"
+        :stroke="progressBarColor"
+        :pathLength="fillerMax"
+        :stroke-dasharray="fillerMax"
+        :stroke-dashoffset="fillerOffset"
+      />
+    </svg>
   </div>
 </template>
 
@@ -105,37 +112,39 @@ export default {
       hovering: false,
       //Set progress bar to 0 initially to trigger transition
       initialized: false,
-      ready: false,
       clockInterval: null,
       date: new Date(),
       secsRemaining: this.secs,
       timerInterval: null,
-      addGt50: false,
-      gt50RenderTimeout: null,
     };
   },
 
   computed: {
+    fillerMax() {
+      return this.perclock || this.perdown ? 60 : 100;
+    },
+
+    fillerOffset() {
+      let max = this.fillerMax;
+      let val = this.renderedPercent;
+
+      if (this.perclock) {
+        val = this.date.getSeconds();
+      }
+
+      if (this.perdown) {
+        val = this.secsRemaining;
+      }
+
+      return max - val;
+    },
+
     renderedPercent() {
       if (!this.initialized) {
         return 0;
       }
 
       return this.percent > 100 ? 100 : this.percent < 0 ? 0 : this.percent;
-    },
-
-    gt50() {
-      let result = false;
-
-      if (this.perclock) {
-        result = this.date.getSeconds() > 30;
-      } else if (this.perdown) {
-        result = this.secsRemaining > 30;
-      } else if (this.renderedPercent || this.displayTextAtZero) {
-        result = this.renderedPercent > 50;
-      }
-
-      return result;
     },
 
     displayText() {
@@ -163,20 +172,6 @@ export default {
 
     textColor() {
       return this.hovering ? this.progressBarColor : "";
-    },
-
-    rotationDegrees() {
-      if (!this.initialized) {
-        return 0;
-      }
-
-      if (this.perclock) {
-        return 6 * this.date.getSeconds();
-      } else if (this.perdown) {
-        return 6 * this.secsRemaining;
-      }
-
-      return 3.6 * this.renderedPercent;
     },
   },
 
@@ -219,28 +214,6 @@ export default {
         }
       },
     },
-
-    gt50: {
-      immediate: true,
-      handler(val) {
-        //Handling the annoying flip between gt50's styles
-        //Could probably be improved if the circle was converted to an SVG - one grey circle with one overlaid progress circle
-        if (!val) {
-          //Wait until bar animation completes, then flip off the fill
-          this.gt50RenderTimeout = setTimeout(() => {
-            this.addGt50 = val;
-            this.gt50RenderTimeout = null;
-          }, 220);
-        } else {
-          if (this.gt50RenderTimeout) {
-            clearTimeout(this.gt50RenderTimeout);
-            this.gt50RenderTimeout = null;
-          }
-
-          this.addGt50 = val;
-        }
-      },
-    },
   },
 
   methods: {
@@ -260,10 +233,6 @@ export default {
       setTimeout(() => {
         //Display start up animation
         this.initialized = true;
-
-        setTimeout(() => {
-          this.ready = true;
-        }, 150);
       }, 0);
     },
 
@@ -348,34 +317,18 @@ export default {
 .percircle {
   position: relative;
   font-size: 120px;
-  width: 1em;
-  height: 1em;
-  -webkit-border-radius: 50%;
-  -moz-border-radius: 50%;
-  -ms-border-radius: 50%;
-  -o-border-radius: 50%;
-  border-radius: 50%;
+  width: 1.3em;
+  height: 1.3em;
   margin: 0 auto;
   background: transparent;
 
-  *,
-  *:before {
-    -webkit-box-sizing: content-box;
-    -moz-box-sizing: content-box;
-    box-sizing: content-box;
+  svg {
+    transform: rotate(-90deg);
   }
 
   &.animate {
-    .bar {
-      transition: transform 0.2s ease-in-out, border-width 0.2s ease-in-out;
-    }
-
-    &:before,
-    .fill {
-      transition: border-width 0.2s ease-in-out;
-    }
-
-    & > span {
+    & > span,
+    .circle-bar {
       transition: all 0.2s ease-in-out;
     }
   }
@@ -394,28 +347,8 @@ export default {
     color: rgba(52, 59, 62, 0.5);
   }
 
-  &:before {
-    content: "";
-    border-radius: 50%;
-    border: solid rgba(0, 0, 0, 0.1);
-    position: absolute;
-  }
-
-  .slice {
-    position: absolute;
-    width: 1em;
-    height: 1em;
-    clip: rect(0, 1em, 1em, 0.5em);
-  }
-
   &:hover {
     cursor: default;
-
-    &:before,
-    .bar,
-    .fill {
-      border-width: 0.04em;
-    }
 
     & > span {
       -webkit-transform: scale(1.3);
@@ -425,53 +358,10 @@ export default {
       transform: scale(1.3);
       color: #307bbb;
     }
-  }
 
-  &:before,
-  .bar,
-  .fill {
-    border-width: 0.08em;
-    width: 1em;
-    height: 1em;
-    box-sizing: border-box;
-  }
-
-  .fill {
-    -webkit-transform: rotate(0deg);
-    -moz-transform: rotate(0deg);
-    -ms-transform: rotate(0deg);
-    -o-transform: rotate(0deg);
-    transform: rotate(0deg);
-  }
-
-  .bar,
-  .fill {
-    position: absolute;
-    border: 0.08em solid #307bbb;
-    clip: rect(0, 0.5em, 1em, 0);
-    -webkit-border-radius: 50%;
-    -moz-border-radius: 50%;
-    -ms-border-radius: 50%;
-    -o-border-radius: 50%;
-    border-radius: 50%;
-  }
-
-  .bar {
-    -moz-backface-visibility: hidden;
-    -webkit-backface-visibility: hidden;
-    backface-visibility: hidden;
-  }
-
-  &.gt50 .fill {
-    -webkit-transform: rotate(180deg);
-    -moz-transform: rotate(180deg);
-    -ms-transform: rotate(180deg);
-    -o-transform: rotate(180deg);
-    transform: rotate(180deg);
-  }
-
-  &.initialized.gt50 .slice {
-    clip: rect(auto, auto, auto, auto);
+    .circle-bar {
+      stroke-width: 5;
+    }
   }
 
   &.small {
@@ -487,18 +377,21 @@ export default {
       color: #ffffff;
     }
 
-    &:before {
-      border-color: rgba(255, 255, 255, 0.7);
+    .filler-bar {
+      stroke: rgba(255, 255, 255, 0.7);
     }
+  }
+
+  .filler-bar {
+    stroke: rgba(0, 0, 0, 0.1);
+  }
+
+  .circle-bar {
+    stroke-width: 10;
   }
 }
 
 .perclock > span {
-  font-size: 0.175em;
-}
-
-.perclock.animate .bar,
-.perdown.animate .bar {
-  transition: border-width 0.2s ease-in-out;
+  font-size: 0.15em;
 }
 </style>
