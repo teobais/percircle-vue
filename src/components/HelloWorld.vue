167ba8d2c92b756fd636e38105e6a6f59eaf2e3b
Richard Saseun <richardsaseun@gmail.com>
2022-03-11 01:10:32 +0100

    chore: add snippet section component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,12 +2,9 @@
   <div class="container">
     <div class="mb-5">
       <h3 class="text-center">Percircle (Custom colors and reactive percents)</h3>
-      <ssh-pre class="mb-4" language="html" copy-button @copied="onCopySuccess" dark reactive>
-        <template #copy-button>
-          <span class="small text-white">{{ copyText }}</span>
-        </template>
+      <SnippetSection class="mb-4">
         &lt;percircle :percent="{{ percent }}" :displayTextAtZero="true" progressBarColor="#1B75BC"/&gt;
-      </ssh-pre>
+      </SnippetSection>
       <percircle :percent="percent" progressBarColor="#1B75BC" />
       <div class="text-center mt-4">
         <b-button variant="outline-secondary" @click="add(10)">Add 10</b-button>
@@ -20,12 +17,9 @@
         Percircle (Text based, small size, dark background, uses same percent as above but translates it to a letter
         grade)
       </h3>
-      <ssh-pre class="mb-4" language="html" copy-button @copied="onCopySuccess" dark reactive>
-        <template #copy-button>
-          <span class="small text-white">{{ copyText }}</span>
-        </template>
+      <SnippetSection class="mb-4">
         &lt;percircle :percent="{{ percent }}" :displayTextAtZero="true" text="{{ letterGrade }}"/&gt;
-      </ssh-pre>
+      </SnippetSection>
       <div class="dark-background">
         <percircle class="small dark" :percent="percent" :text="letterGrade" />
       </div>
@@ -37,35 +31,24 @@
 
     <div class="mb-5">
       <h3 class="text-center">Perdown (Countdown timer, big size, clicking resets it)</h3>
-      <ssh-pre class="mb-4" language="html" copy-button @copied="onCopySuccess" dark reactive>
-        <template #copy-button>
-          <span class="small text-white">{{ copyText }}</span>
-        </template>
+      <SnippetSection class="mb-4">
         &lt;percircle :perdown="true" :secs="{{ secs }}" timeUpText="Done" :resetOnClick="true"/&gt;
-      </ssh-pre>
+      </SnippetSection>
       <percircle class="big" perdown :secs="secs" timeUpText="Done" />
     </div>
 
     <div class="mb-5">
       <h3 class="text-center">Perclock (Clock)</h3>
-      <ssh-pre class="mb-4" language="html" copy-button @copied="onCopySuccess" dark reactive>
-        <template #copy-button>
-          <span class="small text-white">{{ copyText }}</span>
-        </template>
-        &lt;percircle :perclock="true"/&gt;
-      </ssh-pre>
+      <SnippetSection class="mb-4"> &lt;percircle :perclock="true"/&gt; </SnippetSection>
       <percircle perclock />
     </div>
 
     <div class="mb-5">
       <h3 class="text-center">Percircle (Animate on scroll)</h3>
-      <ssh-pre class="mb-4" language="html" copy-button @copied="onCopySuccess" dark reactive>
-        <template #copy-button>
-          <span class="small text-white">{{ copyText }}</span>
-        </template>
+      <SnippetSection class="mb-4">
         &lt;percircle :percent="{{ percent }}" :displayTextAtZero="true" progressBarColor="#1B75BC"
         :animateOnScroll="true"/&gt;
-      </ssh-pre>
+      </SnippetSection>
       <percircle :percent="percent" progressBarColor="#1B75BC" animateOnScroll />
       <div class="text-center mt-4">
         <b-button variant="outline-secondary" @click="add(10)">Add 10</b-button>
@@ -75,12 +58,9 @@
 
     <div class="mb-5">
       <h3 class="text-center">Percircle (Animation off)</h3>
-      <ssh-pre class="mb-4" language="html" copy-button @copied="onCopySuccess" dark reactive>
-        <template #copy-button>
-          <span class="small text-white">{{ copyText }}</span>
-        </template>
+      <SnippetSection class="mb-4">
         &lt;percircle :percent="{{ percent }}" :displayTextAtZero="true" :animate="false"/&gt;
-      </ssh-pre>
+      </SnippetSection>
       <percircle :percent="percent" :animate="animate" />
       <div class="text-center mt-4">
         <b-button variant="outline-secondary" @click="add(10)">Add 10</b-button>
@@ -91,19 +71,18 @@
 </template>
 
 <script>
-import SshPre from 'simple-syntax-highlighter'
 import Percircle from './percircle.vue'
+import SnippetSection from './SnippetSection.vue'
 export default {
   name: 'HelloWorld',
 
   components: {
     Percircle,
-    SshPre,
+    SnippetSection,
   },
 
   data() {
     return {
-      copyText: 'Copy',
       percent: 60,
       secs: 35,
       animate: false,
@@ -135,13 +114,6 @@ export default {
   methods: {
     add(amount) {
       this.percent += amount
-    },
-
-    onCopy() {
-      this.copyText = 'Copied!'
-      setTimeout(() => {
-        this.copyText = 'Copy'
-      }, 2000)
     },
   },
 }
